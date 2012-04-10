cfba5dce12b32cbd54391d21892ab715985d4c4c
Dung Nguyen <clemenation@yahoo.com>
2012-04-10 17:28:40 +0700

    Implement Secure; All CRUD classes now required login; Can login with anything now;

--- app/controllers/InternAddresses.java
+++ app/controllers/InternAddresses.java
@@ -5,6 +5,7 @@ import play.mvc.*;
 
 import models.InternAddress;
 
+@With(Secure.class)
 @CRUD.For(InternAddress.class)
 public class InternAddresses extends CRUD {
 
--- app/controllers/InternCities.java
+++ app/controllers/InternCities.java
@@ -5,6 +5,7 @@ import play.mvc.*;
 
 import models.InternCity;
 
+@With(Secure.class)
 @CRUD.For(InternCity.class)
 public class InternCities extends CRUD {
 
